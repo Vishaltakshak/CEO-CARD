17a07bae64945d358ef069a81fece5b19e652ce1
Vishaltakshak <127527299+Vishaltakshak@users.noreply.github.com>
2025-01-03 23:15:50 +0530

    1.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
 /** @type {import('tailwindcss').Config} */
-// tailwind.config.js
 module.exports = {
-    content: [
-      "./src/**/*.{js,jsx,ts,tsx}", // Specify where Tailwind should look for class names
-    ],
-    theme: {
-      extend: {
-        backgroundImage: {
-          clouds: "url('/public/assets/img/Home/clouds.jpg')",
-          about: "url('/public/assets/img/About/aboutus.jpg')",
-          enquiry: "url('/public/assets/img/Enquiry/enquiry.jpg')",
-          signin: "url('/public/assets/img/signup/signin.jpg')",
-        },
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Specify where Tailwind should look for class names
+  ],
+  theme: {
+    extend: {
+      screens: {
+        xs: "345px", // Custom screen size for 345px
+      },
+      backgroundImage: {
+        clouds: "url('/assets/img/Home/clouds.jpg')",
+        about: "url('/assets/img/About/aboutus.jpg')",
+        enquiry: "url('/assets/img/Enquiry/enquiry.jpg')",
+        signin: "url('/assets/img/signup/signin.jpg')",
       },
     },
-    plugins: [],
-  };
+  },
+  plugins: [],
+};

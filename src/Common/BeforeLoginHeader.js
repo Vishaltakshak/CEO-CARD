1773948b9b6245fed00f3dc74253c09e909a8847
Vishaltakshak <127527299+Vishaltakshak@users.noreply.github.com>
2025-01-04 23:10:04 +0530

    1.31

--- src/Common/BeforeLoginHeader.js
+++ src/Common/BeforeLoginHeader.js
@@ -1,69 +1,74 @@
-import React from "react";
-// import "../css/home.css";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-const BeforLoginHeader = (props) => {
+const BeforeLoginHeader = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
   return (
-    <>
-      <nav className="bg-transparent py-4 absolute z-[100] w-full">
-        <div className="container mx-auto px-4">
-          <div className="flex justify-between items-center">
-            <Link className="navbar-brand" to="/#">
-              <img
-                className="w-[4rem]"
-                src="././assets/img/ceo-logo-white.png"
-                alt="Logo"
-              />
-            </Link>
+    <nav className="bg-transparent py-4 absolute z-[100] w-full">
+      <div className="container mx-auto px-4">
+        <div className="flex justify-between items-center">
+          <Link className="navbar-brand" to="/#">
+            <img
+              className="w-[4rem]"
+              src="././assets/img/ceo-logo-white.png"
+              alt="Logo"
+            />
+          </Link>
 
-            <button
-              className="lg:hidden text-white"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarTogglerDemo01"
-              aria-controls="navbarTogglerDemo01"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
+          <button
+            className="lg:hidden text-white"
+            type="button"
+            onClick={toggleMenu}
+            aria-label="Toggle navigation"
+          >
+            <svg 
+              className="w-6 h-6" 
+              fill="currentColor" 
+              viewBox="0 0 20 20"
             >
-              <span className="navbar-toggler-icon">
-                <i className="bi bi-list" aria-hidden="true"></i>
-              </span>
-            </button>
+              <path 
+                fillRule="evenodd" 
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
 
-            <div className="hidden lg:flex space-x-6">
-              <ul className="flex space-x-6">
-                <li>
-                  <Link
-                    className="text-white font-semibold hover:text-gray-300"
-                    aria-current="page"
-                    to="/about_us"
-                  >
-                    About us
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    className="text-white font-semibold hover:text-gray-300"
-                    to="/LoginPage"
-                  >
-                    Sign in
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    className="text-white font-semibold hover:text-gray-300"
-                    to="/signupnew"
-                  >
-                    Join
-                  </Link>
-                </li>
-              </ul>
-            </div>
+          <div className={`lg:flex ${isOpen ? 'block' : 'hidden'} absolute lg:relative top-full left-0 w-full lg:w-auto bg-transparent mt-2 lg:mt-0 p-4 lg:p-0`}>
+            <ul className="flex flex-col lg:flex-row space-y-2 lg:space-y-0 lg:space-x-6">
+              <li>
+                <Link
+                  className="text-white font-semibold hover:text-gray-300 block"
+                  to="/about_us"
+                >
+                  About us
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-white font-semibold hover:text-gray-300 block"
+                  to="/LoginPage"
+                >
+                  Sign in
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-white font-semibold hover:text-gray-300 block"
+                  to="/signupnew"
+                >
+                  Join
+                </Link>
+              </li>
+            </ul>
           </div>
         </div>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 };
 
-export default BeforLoginHeader;
+export default BeforeLoginHeader;

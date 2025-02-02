d02493a2bdb1dd6d6fe54048b7d15a4d49aee25b
Vishaltakshak <127527299+Vishaltakshak@users.noreply.github.com>
2025-02-02 23:33:34 +0530

    2.3

--- src/Components/BrowseHotelsTopBanner.js
+++ src/Components/BrowseHotelsTopBanner.js
@@ -2,52 +2,51 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import '../css/browsehotelstopbanner.css';
+
 const BrowseHotelsTopBanner = () => {
+  // State for selected destination dropdown in mobile
+  const [isselectdestination, setSelectDestination] = useState(false);
+  const handleSelectDestination = () => {
+    setSelectDestination(!isselectdestination);
+  };
 
-	  //state for selectd destination dropdown in mobile
-	  const [isselectdestination, setSelectDestination] = useState("false");
-	  const handleSelectDestination = () => {
-		setSelectDestination(!isselectdestination);
-	  };
-	return (
-		
-		<>
-			<div className="container-fluid browse-hotels-banner-container">
-				<div className="row browse-hotel-row">
-					<div className="col-xl-12 col-lg-12 col-md-12 col-12 hotels-banner">
-					<div className="absolute top-0 left-0 w-full h-[35.6vh] md:h-[65.6vh] bg-black opacity-30 z-10"></div>
-						<img
-						className="absolute  md:block h-[39vh]  w-[100%] lg:h-[65vh] object-cover"
-						style={{top:'0.25rem'}}
-						src={`${process.env.PUBLIC_URL}/assets/img/Home/tajhotel.jpg`}
-						alt="hotel"
-						/>
-						<div className="absolute top-[30%] right-0 left-0 mr-auto ml-auto text-center z-20">
-						<div className="hotels-banner-text">
-							<h1 className="hotels-banner-title">2024 FC HOTEL COLLECTION</h1>
-							<h2 className="hotels-banner-subtitle">Exclusive Member Rates and Privileges at 500+ Hotels and Resorts</h2>
-							<p className="hotels-banner-para">Earn Eligible Loyalty Points while Booking Directly with Privately Negotiated Rates</p>
+  return (
+    <>
+      <div className="container-fluid browse-hotels-banner-container">
+        <div className="row browse-hotel-row">
+          <div className="col-xl-12 col-lg-12 col-md-12 col-12 hotels-banner relative">
+            {/* Overlay */}
+            <div className="absolute top-0 left-0 w-full h-full bg-black opacity-30 z-10"></div>
+            
+            {/* Background Image */}
+            <img
+              className="absolute inset-0 h-full w-full object-cover"
+              src={`${process.env.PUBLIC_URL}/assets/img/Home/tajhotel.jpg`}
+              alt="hotel"
+            />
 
+            {/* Text Content */}
+            <div className="absolute top-[30%] right-0 left-0 mx-auto text-center z-20">
+              <div className="hotels-banner-text">
+                <h1 className="hotels-banner-title">2024 FC HOTEL COLLECTION</h1>
+                <h2 className="hotels-banner-subtitle">
+                  Exclusive Member Rates and Privileges at 500+ Hotels and Resorts
+                </h2>
+                <p className="hotels-banner-para">
+                  Earn Eligible Loyalty Points while Booking Directly with Privately Negotiated Rates
+                </p>
+              </div>
+            </div>
 
-							{/* <div className="hotel-browse-search-box">
-								<span style={{position:"relative"}}>
-									<img alt="Search" className="search-image"  src="././assets/img/partners/custom-search-icon.svg" />
-									<input className="search-hotel-textbox" type="text" placeholder="Search All Hotels"  />
-								</span>
-							</div> */}
-						</div>
-						</div>
-
-						<div className="hotels-banner-overlay hidden md:block">
-							Featured FC Hotel Property: <Link to="/#">Maroma, A Belmond Hotel Riviera Maya</Link>
-						</div>
-					</div>
-					</div>
-				</div>
-			
-			
-		</>
-	);
+            {/* Featured Hotel Text */}
+            <div className="hotels-banner-overlay hidden md:block">
+              Featured FC Hotel Property: <Link to="/#">Maroma, A Belmond Hotel Riviera Maya</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default BrowseHotelsTopBanner;

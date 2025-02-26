4ed3a58e364533b83dd07255c1afda5346f6159f
Vishaltakshak <127527299+Vishaltakshak@users.noreply.github.com>
2025-02-26 12:41:53 +0530

    final

--- src/Components/pages/ExploreElite.js
+++ src/Components/pages/ExploreElite.js
@@ -6,7 +6,7 @@ export const ExploreElite = () => {
     <>
     <AfterLoginLayout>
       
-        <div className='Containter-fluid bg-black explore-elite-con'>
+        <div className=' bg-white text-black explore-elite-con'>
             <div className='container pe-0 ps-0'>
                 <div className='col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 text-center flex justify-center items-center flex-col explore-elite-parent'>
                     <h2>Experience Exclusive Privileges</h2>
@@ -15,14 +15,14 @@ export const ExploreElite = () => {
                     <div className='Hero-con-img'>
                         
                     </div>
-                    <div className='Explore-elite-text-con'>
-                      <h2 className='text-center'>Get Primary Key Features</h2>
+                    <div className='Explore-elite-text-con bg-white text-black'>
+                      <h2 className='text-center text-black'>Get Primary Key Features</h2>
                       <ul>
-                        <li> <strong>Rewards Program </strong>- 5x points on travel, cashback offers and many more rewards awaiting your way.</li>
-                        <li> <strong>Travel Perks</strong> - Highlight lounge access, free upgrades, or priority boarding for flights</li>
-                        <li><strong>Concierge Services</strong> - Personalized services for bookings, reservations, or event planning.</li>
-                        <li><strong>Exclusive Offers</strong> - Deals or offers on high-end brands, travel experiences, and dining.</li>
-                        <li><strong>Security Features</strong> - Fraud protection, zero-liability policies, and enhanced online safety features.</li>
+                        <li className='text-gray-700'> <strong className='text-black'>Rewards Program </strong>- 5x points on travel, cashback offers and many more rewards awaiting your way.</li>
+                        <li className='text-gray-700'> <strong className='text-black'>Travel Perks</strong> - Highlight lounge access, free upgrades, or priority boarding for flights</li>
+                        <li className='text-gray-700'><strong className='text-black'>Concierge Services</strong> - Personalized services for bookings, reservations, or event planning.</li>
+                        <li className='text-gray-700'><strong className='text-black'>Exclusive Offers</strong> - Deals or offers on high-end brands, travel experiences, and dining.</li>
+                        <li className='text-gray-700'><strong className='text-black'>Security Features</strong> - Fraud protection, zero-liability policies, and enhanced online safety features.</li>
                       </ul>
             
 

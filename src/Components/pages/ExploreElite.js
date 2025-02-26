bb637a63b8c89c913974c1b37751eab01f6f2bf8
Vishaltakshak <127527299+Vishaltakshak@users.noreply.github.com>
2025-02-26 12:44:02 +0530

    final

--- src/Components/pages/ExploreElite.js
+++ src/Components/pages/ExploreElite.js
@@ -9,9 +9,9 @@ export const ExploreElite = () => {
         <div className=' bg-white text-black explore-elite-con'>
             <div className='container pe-0 ps-0'>
                 <div className='col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 text-center flex justify-center items-center flex-col explore-elite-parent'>
-                    <h2>Experience Exclusive Privileges</h2>
+                    <h2 className='text-black'>Experience Exclusive Privileges</h2>
                     
-                    <h5>The ultimate card for luxury, rewards, and perks.</h5>
+                    <h5 className='text-black'>The ultimate card for luxury, rewards, and perks.</h5>
                     <div className='Hero-con-img'>
                         
                     </div>
